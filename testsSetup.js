befbe603d3db843f354f5dedf3a50bfa31b56304
Julie Broderick <jbroderick@labelinsight.com>
2025-05-12 19:28:53 -0500

    test config

--- testsSetup.js
+++ testsSetup.js
@@ -3,33 +3,33 @@ import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
 import mockToDos from './src/mocks/mockToDos'
 
-let originalLocalStorage
+// let originalLocalStorage
 
-beforeAll(() => {
-  originalLocalStorage = window.localStorage
-  window.localStorage = {
-    store: {
-      todos: JSON.stringify(mockToDos),
-    },
-    getItem(key) {
-      return this.store[key] ?? null
-    },
-    setItem(key, value) {
-      this.store[key] = value
-    },
-    removeItem(key) {
-      delete this.store[key]
-    },
-    clear() {
-      this.store = {}
-    },
-  }
-})
+// beforeAll(() => {
+//   originalLocalStorage = window.localStorage
+//   window.localStorage = {
+//     store: {
+//       todos: JSON.stringify(mockToDos),
+//     },
+//     getItem(key) {
+//       return this.store[key] ?? null
+//     },
+//     setItem(key, value) {
+//       this.store[key] = value
+//     },
+//     removeItem(key) {
+//       delete this.store[key]
+//     },
+//     clear() {
+//       this.store = {}
+//     },
+//   }
+// })
 
-afterAll(() => {
-  window.localStorage = originalLocalStorage
-})
+// afterAll(() => {
+//   window.localStorage = originalLocalStorage
+// })
 
-afterEach(() => {
-  cleanup()
-})
+// afterEach(() => {
+//   cleanup()
+// })
